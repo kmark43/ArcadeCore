f70ede15f5fcdcc9bdab685ab3df06095e7de673
Kyler Markland <indekysive@mutinies.net>
2019-03-11 12:38:33 -0700

    Updated no build and no inventory change modules to ignore creative

--- src/main/java/net/mutinies/arcadecore/modules/prevent/NoInventoryChange.java
+++ src/main/java/net/mutinies/arcadecore/modules/prevent/NoInventoryChange.java
@@ -1,6 +1,7 @@
 package net.mutinies.arcadecore.modules.prevent;
 
 import net.mutinies.arcadecore.module.Module;
+import org.bukkit.GameMode;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.player.PlayerDropItemEvent;
@@ -8,11 +9,15 @@ import org.bukkit.event.player.PlayerDropItemEvent;
 public class NoInventoryChange implements Module {
     @EventHandler
     public void onInventoryClick(InventoryClickEvent e) {
-        e.setCancelled(true);
+        if (e.getWhoClicked().getGameMode() != GameMode.CREATIVE) {
+            e.setCancelled(true);
+        }
     }
     
     @EventHandler
     public void onItemDrop(PlayerDropItemEvent e) {
-        e.setCancelled(true);
+        if (e.getPlayer().getGameMode() != GameMode.CREATIVE) {
+            e.setCancelled(true);
+        }
     }
 }
